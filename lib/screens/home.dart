daba115dedbb5e970227d2b52c29a50e58c32360
Jiss Joy <jissjoy3950@gmail.com>
2021-09-17 19:56:26 +0530

    1. main.dart -> Change home route from home.dart to index.dart. | 2. home.dart -> Remove bottom navigation bar. | 3. pubspec.yaml -> Add dependencies.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
