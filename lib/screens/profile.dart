74a10910109f79b04c743559c2a96bd9c24b3ec8
Jiss Joy <jissjoy3950@gmail.com>
2021-09-20 13:53:07 +0530

    1. profile.dart -> Add profile-items and bottom references.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,11 +1,109 @@
+import 'package:brookmobiles/consts/icons.dart';
+import 'package:brookmobiles/widgets/profile_item.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
+  void _goToScreen() {
+    print('OK');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile"),
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            bottom: 100,
+          ),
+          child: Column(
+            children: [
+              ProfileItem(
+                icon: MyIcons.orders,
+                title: 'My Orders',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.services,
+                title: 'My Services',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.cart,
+                title: 'My Cart',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.favorites_o,
+                title: 'My Wishlist',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.profile,
+                title: 'My Profile',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.settings,
+                title: 'Settings',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.legal,
+                title: 'Legal',
+                function: _goToScreen,
+              ),
+              ProfileItem(
+                icon: MyIcons.help,
+                title: 'Help Centre',
+                function: _goToScreen,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Brook Mobiles 2021'),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    MyIcons.whatsapp,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    MyIcons.instagram,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    MyIcons.facebook,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    MyIcons.twitter,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
