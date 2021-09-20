57cd365e9c205cdbc7323c5011d3ab13486e229c
Jiss Joy <jissjoy3950@gmail.com>
2021-09-20 13:41:39 +0530

    1. icons.dart -> Add more Icons.

--- lib/consts/icons.dart
+++ lib/consts/icons.dart
@@ -2,10 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class MyIcons {
+  //Navbar
   static IconData home = FontAwesome.eercast;
   static IconData search = Feather.search;
   static IconData service = Entypo.tools;
   static IconData profile = Feather.user;
+
+  //Toolbar
   static IconData cart = Feather.shopping_cart;
-  static IconData favorites = Feather.heart;
+  static IconData favorites_o = Feather.heart;
+  static IconData favorites = FontAwesome.heart;
+
+  //Profile Screen
+  static IconData orders = SimpleLineIcons.social_dropbox;
+  static IconData services = SimpleLineIcons.wrench;
+  static IconData legal = FontAwesome.file_text_o;
+  static IconData settings = Feather.settings;
+  static IconData help = Feather.headphones;
+
+  //Social Media
+  static IconData whatsapp = FontAwesome.whatsapp;
+  static IconData instagram = FontAwesome.instagram;
+  static IconData facebook = FontAwesome.facebook;
+  static IconData twitter = FontAwesome.twitter;
 }
