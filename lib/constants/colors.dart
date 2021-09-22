e03db33129dc515c4e50c87a78bcc65dcc783336
Jiss Joy <jissjoy3950@gmail.com>
2021-09-22 16:28:53 +0530

    1. Create and add color constants.

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color primary = Color(0XFFFFAB00);
+  static Color primaryMedium = Color(0XFFFFCF6D);
+  static Color primaryLight = Color(0XFFFFC6E3);
+
+  static Color secondary = Color(0XFF344955);
+  static Color secondaryLight = Color(0XFF658DA3);
+
+  static Color accentColor = Colors.black;
+  static Color backgroundColor = Color(0XFFF3F0E9);
+}
