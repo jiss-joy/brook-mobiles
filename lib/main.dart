e340981dee88904dfbc5ba5bb867d08349d4ea5b
Jiss Joy <jissjoy3950@gmail.com>
2021-09-29 12:23:13 +0530

    main.dart -> 1. Change from Stateless to Stateful Widget| 2. Move theme data to separate file. | 3. Add provider class to get data. | 4. Set app theme based on the saved preference.

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,56 @@
-import 'package:brookmobiles/screens/home.dart';
+import 'package:brookmobiles/constants/theme_data.dart';
+import 'package:brookmobiles/providers/dark_theme.dart';
 import 'package:brookmobiles/screens/index.dart';
 import 'package:brookmobiles/screens/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider darkThemeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    // Fetches the saved theme preference.
+    darkThemeProvider.darkTheme =
+        await darkThemeProvider.darkThemePreferences.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Brook Mobiles',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0XFFFFF6E3),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        primaryColor: Color(0XFFFFAB00),
-        accentColor: Colors.black,
-        primaryColorLight: Color(0XFFFFCF6D),
-        splashColor: Color(0XFFFFCF6D),
-        backgroundColor: Color(0XFFF3F0E9),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(
-          //Raleway-SemiBold
-          headline5: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontFamily: "Raleway",
-          ),
-          //Raleway-Bold
-          headline6: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontFamily: "Raleway",
-          ),
-          //Montserrat-Medium
-          subtitle1: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontFamily: "Montserrat",
-          ),
-          //Montserrat-Regular
-          bodyText1: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontFamily: "Montserrat",
-          ),
-          //Montserrat-Thin
-          bodyText2: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontFamily: "Montserrat",
-          ),
-        ),
+    return MultiProvider(
+      providers: [
+        //To provide the theme data.
+        ChangeNotifierProvider(create: (_) {
+          return darkThemeProvider;
+        })
+      ],
+      child: Consumer<DarkThemeProvider>(
+        builder: (context, themeData, child) {
+          return MaterialApp(
+            title: 'Brook Mobiles',
+            theme: Styles.themeData(darkThemeProvider.darkTheme, context),
+            home: IndexScreen(),
+            routes: {
+              SettingsScreen.routeName: (ctx) => SettingsScreen(),
+            },
+          );
+        },
       ),
-      home: IndexScreen(),
-      routes: {
-        SettingsScreen.routeName: (ctx) => SettingsScreen(),
-      },
     );
   }
 }
